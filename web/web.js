5aa4aaf16111d11228efd8c74c0c2de6aafa1c53
Duc <48994241+nduc03@users.noreply.github.com>
2021-08-18 13:43:12 +0700

    some web changing

--- web/web.js
+++ web/web.js
@@ -8,17 +8,17 @@ const port = process.env.PORT || 80
 app.use(express.static('web/static'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    fs.createReadStream(path.join(__dirname, 'html/index.html')).pipe(res)
 })
 
-app.get('/term_vi',(req, res) => {
+app.get('/term_vi', (req, res) => {
     fs.createReadStream(path.join(__dirname, 'html/term_vi.html')).pipe(res)
 })
 
-app.get('/privacy_vi',(req, res) => {
+app.get('/privacy_vi', (req, res) => {
     fs.createReadStream(path.join(__dirname, 'html/privacy_vi.html')).pipe(res)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
